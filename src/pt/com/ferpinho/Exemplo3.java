809a6d42a2120467951444ab5f8771cd356d01b2
Fernando Pinho <fernandoalbertopinho20@gmail.com>
2023-11-16 21:43:38 +0100

    enviar outra vez

--- src/pt/com/ferpinho/Exemplo3.java
+++ src/pt/com/ferpinho/Exemplo3.java
@@ -2,7 +2,7 @@ package pt.com.ferpinho;
 
 public class Exemplo3 {
     public static void main(String args[]) {
-        Integer idade = 30;
-        System.out.println(idade);
+        int num1 = 50;
+        Integer numL = Integer.valueOf(num1);
     }
 }
