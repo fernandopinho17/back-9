c483ef2501130a9d6930f04a15c44acc1b727067
Fernando Pinho <fernandoalbertopinho20@gmail.com>
2023-11-16 20:43:59 +0100

    enviar novamente

--- src/pt/com/ferpinho/Exemplo3.java
+++ src/pt/com/ferpinho/Exemplo3.java
@@ -1,13 +1,8 @@
 package pt.com.ferpinho;
 
 public class Exemplo3 {
-   public static void main(String args[]) {
-       int num1 = 20;
-       short num2 = (short) num1;
-
-       long numL = 22222222;
-       System.out.println(numL);
-       int numI = (int) numL;
-       System.out.println(numI);
-   }
+    public static void main(String args[]) {
+        Integer idade = 30;
+        System.out.println(idade);
+    }
 }
